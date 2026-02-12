c9e7ca023b07112eaaffa4ba4edf80768568bb0a
Vaishanvi Mishra <vaishnavim1105@gmail.com>
2026-02-12 12:41:57 +0530

    arnav mistake solved

--- deptweb/src/app/page.js
+++ deptweb/src/app/page.js
@@ -2,6 +2,7 @@ import DgxCoe from "@/components/home/dgxcoe";
 import PlacementOverview from "@/components/home/placement-overview";
 import PlacementRecord from "@/components/home/placement-record";
 import Overview from "@/components/home/overview";
+import KeyHighlights from "@/components/home/key-highlight";
 import Faculty from "@/components/home/faculty";
 import ProgramDetails from "@/components/home/program-details";
 import PublicationsResearch from "@/components/home/publications-research";
@@ -18,6 +19,7 @@ export default function Home() {
       <PlacementRecord />
       <DgxCoe />
       <PlacementOverview />
+      <KeyHighlights />
       <Faculty />
       <ProgramDetails />
       <PublicationsResearch />
